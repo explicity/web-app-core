8fddac72bf7f3cbbe76584f8e1816895b4a701fa
Olha Yakymovych <msolya@outlook.com>
2020-02-28 23:23:47 +0200

    add tag repository and model

---
+++ server/data/repositories/tag.repository.ts
@@ -0,0 +1,28 @@
+import { EntityRepository } from 'typeorm';
+
+import Tag from '../entities/Tag';
+import BaseRepository from './base.repository';
+import { ITagSearch } from '../../common/models/tag';
+
+@EntityRepository(Tag)
+export default class TagRepository extends BaseRepository<Tag> {
+  getByFilter({ from, count, keyword }: ITagSearch) {
+    const tags = this.createQueryBuilder('tags')
+      .select('tags.id')
+      .addSelect('tags.keyword');
+
+    if (keyword) {
+      tags.where('LOWER(tags.keyword) LIKE LOWER(:keyword)', {
+        keyword: `%${keyword}%`
+      });
+    }
+
+    return tags
+      .skip(from)
+      .take(count)
+      .orderBy({
+        'tags.keyword': 'ASC'
+      })
+      .getMany();
+  }
+}
