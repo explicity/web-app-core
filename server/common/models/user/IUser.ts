a07f7ae3bf543339e92b298d67298289d255d1b1
Olha Yakymovych <olha.yakymovych@binary-studio.com>
2020-05-09 14:17:03 +0300

    minor improvement

--- server/common/models/user/IUser.ts
+++ server/common/models/user/IUser.ts
@@ -1,8 +1,6 @@
 import { IShortUser } from './IShortUser';
 
-import { IRole } from '../role/IRole';
 import { IArticle } from '../article';
-import { INewspaper } from '../newspaper';
 import { IArticleReaction } from '../article-reaction';
 
 export interface IUser extends IShortUser {
