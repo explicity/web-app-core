608d6b627977c4142af8c425532794ef2f7c8b3a
Olha Yakymovych <msolya@outlook.com>
2020-05-26 01:19:11 +0300

    added main title section of extended article

--- client/src/screens/Articles/containers/ArticleView/index.tsx
+++ client/src/screens/Articles/containers/ArticleView/index.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { boundMethod } from 'autobind-decorator';
 import { RouteComponentProps } from 'react-router';
+import { Heading, Box, Button, Text, Grid } from 'grommet';
+import { Favorite, Chat, Share, Clock } from 'grommet-icons';
+import moment from 'moment';
 
 import LoaderWrapper from 'components/LoaderWrapper';
 import { IBindingCallback1 } from 'models/callback';
 
 import { getExtendedArticle } from '../../selectors';
 import { fetchExtendedNewspaperArticle } from '../../routines';
+
+import styles from './styles.module.scss';
 
 interface MatchParams {
   newspaperId: string;
@@ -21,7 +26,7 @@ export interface IArticleViewProps extends RouteComponentProps<MatchParams> {
   }>;
   loading: boolean;
   error: object | string | null;
-  article: any
+  article: any;
 }
 interface IArticleViewState {}
 
@@ -47,8 +52,60 @@ class ArticleView extends React.Component<
   }
 
   render() {
-    console.log(this.props.article);
-    return <div></div>;
+    const { loading, article } = this.props;
+
+    return (
+      <LoaderWrapper loading={loading}>
+        {article && (
+          <Box background='#2E2D30' className={styles.screen}>
+            <Box className={styles.container} margin='large' elevation='small'>
+              <Box className={styles.headerOuter}>
+                <Box className={styles.headerGenreOuter}>
+                  <Text className={styles.headerGenre}>{article.genre}</Text>
+                </Box>
+                <Box className={styles.headerTitleOuter}>
+                  <Heading level='2' className={styles.headerTitle}>
+                    {article.title}
+                  </Heading>
+                  {article.subtitle && (
+                    <Heading level='3' className={styles.headerSubtitle}>
+                      {article.subtitle}
+                    </Heading>
+                  )}
+                </Box>
+                <Grid
+                  className={styles.headerInfoOuter}
+                  columns={['small', 'xsmall', 'small']}
+                  gap='small'
+                >
+                  <Text className={styles.publicationDate}>
+                    <Clock className={styles.publicationDateIcon} />
+                    {moment(article.publicationDate).format('ll')}
+                  </Text>
+                  <Box className={styles.interactionsOuter}>
+                    <Text className={styles.interaction}>
+                      <Chat className={styles.interactionIcon} />
+                      {article.commentCount}
+                    </Text>
+                    <Text className={styles.interaction}>
+                      <Favorite className={styles.interactionIcon} />
+                      {article.likeCount}
+                    </Text>
+                  </Box>
+                  <Button
+                    plain
+                    gap='xsmall'
+                    icon={<Share className={styles.shareButtonIcon} />}
+                    label='Share'
+                    className={`${styles.button} ${styles.shareButton}`}
+                  />
+                </Grid>
+              </Box>
+            </Box>
+          </Box>
+        )}
+      </LoaderWrapper>
+    );
   }
 }
 
@@ -56,11 +113,6 @@ const mapStateToProps = (state, props) => {
   const { articleId } = props.match.params;
 
   const { loading, error } = state.article.requests.extendedArticle;
-  const {
-    user: {
-      profile: { user }
-    }
-  } = state;
 
   return {
     loading,
